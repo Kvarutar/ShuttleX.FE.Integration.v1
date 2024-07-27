51c84425ad1576551d4b618f32ca2f0e73ba139b
Aleksandr-Voronchikhin <aleksandr.voronchikhin@shuttlex.com>
2024-07-27 12:00:20 +0300

    [Task-119-Part2] Replacing fetch with axios (#139)

--- src/core/client/index.ts
+++ src/core/client/index.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { type IAxiosRetryConfig } from 'axios-retry';
 import axiosRetry from 'axios-retry';
 
-const createAxiosInstance = ({ url, retryConfig }: { url?: string; retryConfig?: IAxiosRetryConfig }) => {
+const createAxiosInstance = ({ url, retryConfig }: { url: string; retryConfig?: IAxiosRetryConfig }) => {
   const instance = axios.create({
-    baseURL: url ?? 'https://4d4e13d2-119c-4f9c-b83a-c27f0bd9c90a.mock.pstmn.io/api/v1.0',
+    baseURL: url,
   });
 
   if (retryConfig) {
