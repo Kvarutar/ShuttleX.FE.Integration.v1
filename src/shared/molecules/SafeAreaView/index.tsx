2f101924a391a90121a803076656b33b70498f50
Nick Melnik <97626130+mrbywater@users.noreply.github.com>
2024-10-24 18:02:45 +0300

    [Task-288] Passenger-ride - adaptive (#252)
    * [Task-288] Passenger-ride - adaptive
    
    * fix

--- src/shared/molecules/SafeAreaView/index.tsx
+++ src/shared/molecules/SafeAreaView/index.tsx
@@ -1,18 +1,22 @@
 import { Platform, SafeAreaView as SafeAreaViewBase, StyleSheet, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import sizes from '../../../core/themes/sizes';
-import { useThemeV1 } from '../../../core/themes/v1/themeContext';
+import { useTheme } from '../../../core/themes/v2/themeContext';
 import { type SafeAreaViewProps } from './props';
 
 const SafeAreaView = ({ children, containerStyle, wrapperStyle, withTransparentBackground }: SafeAreaViewProps) => {
-  const { colors } = useThemeV1();
+  const { colors } = useTheme();
+  const insets = useSafeAreaInsets();
+
+  const iosPaddingVertical = insets.bottom ? 0 : sizes.paddingVertical / 2;
 
   const computedStyles = StyleSheet.create({
     wrapper: {
       backgroundColor: withTransparentBackground ? 'transparent' : colors.backgroundPrimaryColor,
     },
     container: {
-      paddingVertical: Platform.OS === 'android' ? sizes.paddingVertical : 0,
+      paddingVertical: Platform.OS === 'android' ? sizes.paddingVertical : iosPaddingVertical,
     },
   });
 
