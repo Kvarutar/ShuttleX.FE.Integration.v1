ed5769922760e49447a3ac4874858da3c23554b4
Aleksandr-Voronchikhin <aleksandr.voronchikhin@shuttlex.com>
2024-11-26 23:22:33 +0300

    [Task-316-part2] Fix keychain on android (#296)
    Co-authored-by: Nikolai141 <nikolai.balantsev@shuttlex.com>

--- src/core/client/utils/saveTokens.ts
+++ src/core/client/utils/saveTokens.ts
@@ -1,10 +1,10 @@
-import Keychain from 'react-native-keychain';
+import Keychain, { STORAGE_TYPE } from 'react-native-keychain';
 
 const saveTokens = async ({ accessToken, refreshToken }: { accessToken: string; refreshToken: string }) => {
   await Keychain.resetGenericPassword();
 
   //TODO: add Keychain Sharing
-  await Keychain.setGenericPassword(accessToken, refreshToken);
+  await Keychain.setGenericPassword(accessToken, refreshToken, { storage: STORAGE_TYPE.FB });
 };
 
 export default saveTokens;
