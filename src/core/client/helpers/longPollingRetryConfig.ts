11b7ae2d4ca96a7b8b205f3a50d21f20f3933bcf
SynytskaK <khrystyna.synytska@shuttlex.com>
2024-12-16 14:24:07 +0000

    324/3/longpoll (#327)
    Co-authored-by: Kristina Synytska <hrustyk@MacBook-Pro.station>

--- src/core/client/helpers/longPollingRetryConfig.ts
+++ src/core/client/helpers/longPollingRetryConfig.ts
@@ -1,8 +1,18 @@
+import { type AxiosError } from 'axios';
 import axiosRetry from 'axios-retry';
 
 const axiosLongPollingRetryConfig = {
   retries: Infinity,
   retryDelay: axiosRetry.exponentialDelay,
+  retryCondition: (error: AxiosError) => {
+    if (error.response?.status === 408) {
+      return true;
+    }
+    if (error.response?.status && error.response.status >= 500 && error.response.status <= 599) {
+      return true;
+    }
+    return false;
+  },
 };
 
 export default axiosLongPollingRetryConfig;
