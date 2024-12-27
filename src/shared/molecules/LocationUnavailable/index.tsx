88bad26cde4b6560c521ae23fc3cae99ba342f20
Nick Melnik <97626130+mrbywater@users.noreply.github.com>
2024-12-27 18:34:11 +0200

    [Task-449] Contractor- “Location disabled” pop-up (#360)
    * [Task-449] Contractor- “Location disabled” pop-up
    
    * fix

--- src/shared/molecules/LocationUnavailable/index.tsx
+++ src/shared/molecules/LocationUnavailable/index.tsx
@@ -2,73 +2,55 @@ import { I18nextProvider, useTranslation } from 'react-i18next';
 import { StyleSheet } from 'react-native';
 
 import i18nIntegration from '../../../core/locales/i18n';
-import { useThemeV1 } from '../../../core/themes/v1/themeContext';
-import Blur from '../../atoms/Blur';
-import ButtonV1 from '../../atoms/Button/v1';
-import Text from '../../atoms/Text';
+import Button from '../../atoms/Button/v2';
+import BigHeader from '../BigHeader';
 import BottomWindow from '../BottomWindow';
 import { type LocationUnavailableProps } from './props';
 
 const LocationUnavailableWithoutI18n = ({ reason, onButtonPress }: LocationUnavailableProps) => {
-  const { colors } = useThemeV1();
   const { t } = useTranslation();
 
-  const computedStyles = StyleSheet.create({
-    title: { color: colors.errorColor },
-  });
-
-  let title = '';
-  let subtitle = '';
+  let firstHeaderTitle = '';
+  let secondHeaderTitle = '';
+  let description = '';
 
   switch (reason) {
     case 'permission_denied': {
-      title = t('LocationUnavailable_permissionDeniedTitle');
-      subtitle = t('LocationUnavailable_permissionDeniedSubtitle');
+      firstHeaderTitle = t('LocationUnavailable_permissionDeniedFirstTitle');
+      secondHeaderTitle = t('LocationUnavailable_permissionDeniedSecondTitle');
+      description = t('LocationUnavailable_permissionDeniedDescription');
       break;
     }
     case 'location_disabled': {
-      title = t('LocationUnavailable_locationDisabledTitle');
-      subtitle = t('LocationUnavailable_locationDisabledSubtitle');
+      firstHeaderTitle = t('LocationUnavailable_locationDisabledFirstTitle');
+      secondHeaderTitle = t('LocationUnavailable_locationDisabledSecondTitle');
+      description = t('LocationUnavailable_locationDisabledDescription');
       break;
     }
     case 'accuracy_reduced': {
-      title = t('LocationUnavailable_accuracyReducedTitle');
-      subtitle = t('LocationUnavailable_accuracyReducedSubtitle');
+      firstHeaderTitle = t('LocationUnavailable_accuracyReducedFirstTitle');
+      secondHeaderTitle = t('LocationUnavailable_accuracyReducedSecondTitle');
+      description = t('LocationUnavailable_accuracyReducedDescription');
       break;
     }
   }
 
   return (
-    <>
-      <Blur />
-      <BottomWindow windowStyle={styles.container}>
-        <Text style={[styles.title, computedStyles.title]}>{title}</Text>
-        <Text style={styles.subtitle}>{subtitle}</Text>
-        <ButtonV1
-          containerStyle={styles.button}
-          text={t('LocationUnavailable_settingsButton')}
-          onPress={onButtonPress}
-        />
-      </BottomWindow>
-    </>
+    <BottomWindow withShade>
+      <BigHeader
+        windowTitle={t('LocationUnavailable_windowTitle')}
+        firstHeaderTitle={firstHeaderTitle}
+        secondHeaderTitle={secondHeaderTitle}
+        description={description}
+      />
+      <Button containerStyle={styles.button} text={t('LocationUnavailable_settingsButton')} onPress={onButtonPress} />
+    </BottomWindow>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-  },
-  title: {
-    fontFamily: 'Inter Medium',
-    fontSize: 18,
-    marginBottom: 8,
-  },
-  subtitle: {
-    textAlign: 'center',
-    marginBottom: 32,
-  },
   button: {
-    alignSelf: 'stretch',
+    marginTop: 90,
   },
 });
 
