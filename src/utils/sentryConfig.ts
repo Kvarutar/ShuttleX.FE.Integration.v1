6e7f204ddc1b3f8994261e9c120443836bfc1efd
Nikolai141 <nikolai.balantsev@shuttlex.com>
2025-01-06 13:15:48 +0300

    [Task-435] Add logger - Part 2 (#371)

--- src/utils/sentryConfig.ts
+++ src/utils/sentryConfig.ts
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/react-native';
 
 export const getDefaultDevSentryConfig = (dsn: string): Sentry.ReactNativeOptions => ({
   dsn,
+  environment: 'dev',
   enableAutoSessionTracking: true,
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
@@ -22,6 +23,7 @@ export const getDefaultDevSentryConfig = (dsn: string): Sentry.ReactNativeOption
 
 export const getDefaultProdSentryConfig = (dsn: string): Sentry.ReactNativeOptions => ({
   dsn,
+  environment: 'prod',
   enableAutoSessionTracking: true,
   tracesSampleRate: 0.2,
   profilesSampleRate: 0.2,
